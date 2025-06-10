ed85209c472237d1d0f82f2d354b77a1ce48eae0
pacific-oo5 <sharshenalievbarni@gmail.com>
2025-06-10 09:48:33 +0600

    added frameworks

--- backendAPI/settings.py
+++ backendAPI/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # framework
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
